447687b96c138f23d9b00232bbc7b7162f9a29c6
thefrox <nicolas.bastian@doyoogo.com>
2019-04-19 18:23:47 +0200

    Add user/login verication and uploader controller /views

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+//Login
+Route::get('/login', 'MainController@index');
+Route::post('/login/checklogin', 'MainController@checklogin');
+Route::get('login/successlogin', 'MainController@successlogin');
+Route::get('login/logout', 'MainController@logout');
+
+//FileUploader
+Route::get('/uploader', 'UploaderController@index');
+Route::get('/uploader/add', 'UploaderController@add');
+Route::get('/uploader/edit', 'UploaderController@edit');
+Route::get('/uploader/list', 'UploaderController@list');
